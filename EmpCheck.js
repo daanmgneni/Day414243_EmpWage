f620ee9e66114e2dd6d804834f6e2132f6cb4699
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-05-13 08:46:16 +0530

    Update EmpCheck.js

--- EmpCheck.js
+++ EmpCheck.js
@@ -1,4 +1,20 @@
-const IS_PART_TIME=1;
+//uc1
+/*const IS_ABSENT=0;
+console.log(Math.random()*10);
+let empCheck=Math.floor(Math.random()*10)%2;
+
+if(empCheck==IS_ABSENT)
+{
+    console.log("Employee is Absent");
+}
+else
+{
+    console.log("Employee is Present");
+}*/
+
+//UC2
+
+/*const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
@@ -22,5 +38,34 @@ switch(empCheck)
 
 }
 
+let empWage=empHrs*WAGE_PER_HOUR;
+console.log("EmpWage:"+empWage);*/
+
+//uc3
+
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOUR=20;
+
+let empHrs=0;
+
+function getWorkingHours(empCheck)
+{
+switch(empCheck)
+{
+    case IS_PART_TIME:
+        return PART_TIME_HOURS;
+
+    case IS_FULL_TIME:
+          return FULL_TIME_HOURS;
+    default:
+        return 0;
+
+}
+}
+let empCheck=Math.floor(Math.random()*10)%3;
+empHrs=getWorkingHours(empCheck);
 let empWage=empHrs*WAGE_PER_HOUR;
 console.log("EmpWage:"+empWage);
