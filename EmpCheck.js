9bc00b39c8799c1fbc187c02dd0d3ba73af4a96c
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-05-13 08:43:39 +0530

    Create EmpCheck.js

---
+++ EmpCheck.js
@@ -0,0 +1,13 @@
+//uc1
+const IS_ABSENT=0;
+console.log(Math.random()*10);
+let empCheck=Math.floor(Math.random()*10)%2;
+
+if(empCheck==IS_ABSENT)
+{
+    console.log("Employee is Absent");
+}
+else
+{
+    console.log("Employee is Present");
+}
